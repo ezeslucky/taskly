3e5951bc5b1bb2d071033cceadc221df18ebb22b
Lucky <lucky123@gmail.com>
2025-08-14 19:59:08 +0530

    devcontainer

--- web/app/components/plugins/plugin-page/index.tsx
+++ web/app/components/plugins/plugin-page/index.tsx
@@ -186,7 +186,7 @@ const PluginPage = ({
               isExploringMarketplace && (
                 <>
                   <Link
-                    href='https://github.com/langgenius/dify-plugins/issues/new?template=plugin_request.yaml'
+                    href='https://github.com/ezeslucky/taskly-plugins/issues/new?template=plugin_request.yaml'
                     target='_blank'
                   >
                     <Button
